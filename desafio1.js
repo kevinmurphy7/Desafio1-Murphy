225bf2a3d251d43b458b09ca4b264f71cfa68eb2
Kevin Murphy <tec.kevinmurphy@gmail.com>
2023-09-12 09:51:25 -0300

    desafio 2

--- desafio1.js
+++ desafio1.js
@@ -1,10 +1,19 @@
+const fs = require("fs");
+
 class ProductManager {
-    constructor (){
-        this.products = []
+    constructor(path) {
+        this.path = path;
+        this.products = [];
+        this.loadProducts();
     }
 
-    getProduct (){
-        return this.products
+    async loadProducts() {
+        try {
+            const productList = await fs.promises.readFile(this.path, "utf-8");
+            this.products = JSON.parse(productList);
+        } catch (error) {
+            this.products = [];
+        }
     }
 
     nuevoId = ()=>{
@@ -16,46 +25,86 @@ class ProductManager {
         }
     }
 
-    addProduct = (title,description,price,thumbnail,code,stock) => {
-        const id =this.nuevoId ()
+    async addProduct (title,description,price,thumbnail,code,stock) {
+        const id =this.nuevoId ();
+
         if (!title || !description || !price || !thumbnail || !code || !stock){
             console.error("Ingresar los datos del producto");
             return
         }
 
-        const existProduct = this.products.find (item =>item.code===code)
+        const existProduct = this.products.find (item =>item.code===code);
+
         if (existProduct){
-            console.error ("El codigo ya existe")
+            console.error ("El codigo ya existe");
+            return;
         }else {
-            this.products.push ({
-                id,
-                title,
-                description,
-                price,
-                thumbnail,
-                code,
-                stock
-            })
+            const newProduct = { id, title, description, price, thumbnail, code, stock };
+            this.products.push(newProduct);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
         }
     }
 
-    getProductById = (id) =>{
-        const prod = this.getProduct ().find (item => item.id === id)
-        if (!prod){
-            console.error ("Not Found");
-            return
-        }else{
-            return prod
+    async upDateProduct(id, title, description, price, thumbnail, code, stock) {
+        const allProducts = await this.getProducts();
+    
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Debes ingresar todos los datos para la actualización.");
+            return;
         }
+    
+        const repetCode = allProducts.find(element => element.code === code);
+    
+        if (repetCode) {
+            console.error("El código que intentas ingresar ya existe.");
+            return;
+        } else {
+            const newProductList = allProducts.map(element => {
+                if (element.id === id) {
+                    const upDateProduct = {
+                        ...element,
+                        title,
+                        description,
+                        price,
+                        thumbnail,
+                        code,
+                        stock
+                    };
+                    return upDateProduct;
+                } else {
+                    return element;
+                }
+            });
+
+        await fs.promises.writeFile(this.path, JSON.stringify(newProductList, null, 2));
+        }
+    }
+    
+    async deleteProduct(id) {
+        const allProducts = await this.getProducts();
+        const productNotFound = allProducts.filter(element => element.id !== id);
+        await fs.promises.writeFile(this.path, JSON.stringify(productNotFound, null, 2));
+    }
+    
+    async getProductById(id) {
+        const allProducts = await this.getProducts();
+        const found = allProducts.find(element => element.id === id);
+        return found;
+    }
+    
+    async getProducts() {
+        const productList = await fs.promises.readFile(this.path, "utf-8");
+        const productListparse = JSON.parse(productList);
+        return productListparse;
     }
 }
 
-const productManager = new ProductManager ();
-productManager.addProduct ("celular", "Iphone 14", 1000, "url", "code1",10)
-productManager.addProduct ("tablet", "Ipad", 1500, "url", "code2",5)
-productManager.addProduct ("notebook", "Macbook", 2000, "url", "code3",12)
-productManager.addProduct ("auriculares", "Airpods", 300, "url", "code4",15)
+async function generator() {
+    const productManager = new ProducManager("./products.json");
 
-console.log(productManager.getProduct());
-console.log( "Producto por id");
-console.log(productManager.getProductById (2));
+    const solo = await productManager.getProductById(2);
+    console.log(solo);
+}
+
+generator();
